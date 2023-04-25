8c8a137bc93ff12396733bca003bed99dad477e6
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-25 07:45:27 -0700

    Add updating remaining character counter for the text area that turns red when negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // display remaining characters
+  const maxChar = 140;
+  $("#tweet-text").on("keyup", function() {
+    let currentChar = $(this).val().length;
+    let remainingChar = maxChar - currentChar;
+    //updates the counter to the current remaining characters
+    let counter = $(this).next().children(".counter").html(remainingChar);
+    // if there are no remaining characters apply the css class to make the negative counter red
+    if (remainingChar < 0) {
+      $(counter).addClass("counter-negative");
+    }
+  });
+});
