ae67e0205ecad82758642a5626789a6181a70fdb
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-26 08:24:45 -0700

    Moved counter logic inside charCounter function and called it, add conditional to remove styling when going from a negative to a positive count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
   // display remaining characters
-  const maxChar = 140;
-  $("#tweet-text").on("keyup", function() {
-    let currentChar = $(this).val().length;
-    let remainingChar = maxChar - currentChar;
-    //updates the counter to the current remaining characters
-    let counter = $(this).next().children(".counter").html(remainingChar);
-    // if there are no remaining characters apply the css class to make the negative counter red
-    if (remainingChar < 0) {
-      $(counter).addClass("counter-negative");
-    }
-  });
+  const charCounter = function() {
+    const maxChar = 140;
+    $("#tweet-text").on("keyup", function() {
+      let currentChar = $(this).val().length;
+      let remainingChar = maxChar - currentChar;
+      //updates the counter to the current remaining characters
+      let counter = $(this).next().children(".counter").html(remainingChar);
+      // if there are no remaining characters apply the css class to make the negative counter red
+      if (remainingChar < 0) {
+        $(counter).addClass("counter-negative");
+      }
+      if (remainingChar >= 0) {
+        $(counter).removeClass("counter-negative");
+      }
+    });
+  };
+
+  charCounter();
 });
