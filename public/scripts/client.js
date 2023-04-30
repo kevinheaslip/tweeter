8a7b9cbe2f31fb2251c0919ad73fd46a240ed956
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-30 00:22:16 -0700

    Add data checks and validation for submitted tweet, display alerts if tweet is longer than 140 chars or null/an empty string

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// // Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 $(document).ready(function() {
   // takes in a tweet object, returns a tweet <article> element containing the entire HTML structure of the tweet
   const createTweetElement = function(tweetData) {
@@ -65,24 +39,28 @@ $(document).ready(function() {
     }
   };
 
-  // renderTweets(data);
-
   $(function() {
     // event listener for form submit to prevent default behaviour
     $("form").on("submit", function(event) {
       console.log('Button clicked, performing ajax call...');
       event.preventDefault();
-      // use jQuery library to make an ajax POST request to the server with the serialized form data
       const data = $(this).serialize();
-      $.post("/tweets/", data, function() {
-        console.log("Success, tweet sent to server");
-      });
+      if ($("#tweet-text").val().length > 140) {
+        alert("Sorry, your tweet is longer than 140 characters!");
+      } else if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
+        alert("You are trying to submit an empty tweet, please try again.");
+      } else {
+        // use jQuery library to make an ajax POST request to the server with the serialized form data
+        $.post("/tweets/", data, function() {
+          console.log("Success, tweet sent to server");
+        });
+      }
     })
   });
 
   const loadTweets = function() {
     $.getJSON("/tweets/", function(data) {
-      console.log("Success, tweets retrived from server");
+      console.log("Success, tweets retrieved from server");
       renderTweets(data);
     });
   };
