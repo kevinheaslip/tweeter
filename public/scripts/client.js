87dc3c709229535e7c6d880324b8553127448f44
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-04-29 23:03:25 -0700

    Remove fake hardcoded data, add timeago formatting to tweetData.created_at in createTweetElement function, remove unnecessary function call, add loadTweets function to get tweets JSON from server using jQuery ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// // Fake data taken from initial-tweets.json
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $(document).ready(function() {
   // takes in a tweet object, returns a tweet <article> element containing the entire HTML structure of the tweet
@@ -45,7 +45,7 @@ $(document).ready(function() {
       <p>${tweetData.content.text}</p>
       <br>
       <footer>
-        <div class="tweetPosted">${tweetData.created_at}</div>
+        <div class="tweetPosted">${timeago.format(tweetData.created_at)}</div>
         <div class="socialIcons">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -65,7 +65,7 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
   $(function() {
     // event listener for form submit to prevent default behaviour
@@ -79,5 +79,14 @@ $(document).ready(function() {
       });
     })
   });
+
+  const loadTweets = function() {
+    $.getJSON("/tweets/", function(data) {
+      console.log("Success, tweets retrived from server");
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 
 });
